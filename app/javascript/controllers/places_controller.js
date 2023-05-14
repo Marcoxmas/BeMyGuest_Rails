d027b0655cfc74068e019dbd491e78e4efc8a088
Gianluca Sanfilippo <sanfilippo.1943663@studenti.uniroma1.it>
2023-05-14 03:27:15 +0200

    perfezionata funzionalità autocompletamento e chiamata get parametrica

--- app/javascript/controllers/places_controller.js
+++ app/javascript/controllers/places_controller.js
@@ -4,8 +4,6 @@ export default class extends Controller {
   static targets = [ "field", "map", "latitude", "longitude" ]
 
   connect() {
-    console.log("hiiiiiii")
-
     if (typeof(google) != "undefined") {
       this.initMap()
     }
@@ -14,16 +12,23 @@ export default class extends Controller {
   initMap() {
     //39.5 -98.35 sono usa per centrare la mappa se non ci sono altre coordinate
     //41.5 12.4 sono roma
+    /*
+      this.data.get("latitude") ||
+      this.data.get("longitude") ||
+    */
     this.map = new google.maps.Map(this.mapTarget, {
-      center: new google.maps.LatLng(this.data.get("latitude") || 41.5, this.data.get("longitude") || 12.4),
+      center: new google.maps.LatLng( 41.5, 12.4),
       zoom: (this.data.get("latitude") == null ? 6 : 15)
     })
 
+
     this.autocomplete = new google.maps.places.Autocomplete(this.fieldTarget)
     this.autocomplete.bindTo('bounds', this.map)
-    this.autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+    this.autocomplete.setFields(['address_components', 'geometry', 'icon', 'name', "formatted_address", "place_id"])
+    this.autocomplete.setTypes(['locality', 'country'])
     this.autocomplete.addListener('place_changed', this.placeChanged.bind(this))
 
+    
     this.marker = new google.maps.Marker({
       map: this.map,
       anchorPoint: new google.maps.Point(0, -29)
@@ -32,6 +37,8 @@ export default class extends Controller {
 
   placeChanged() {
     let place = this.autocomplete.getPlace()
+
+    console.log(place)
 
     if (!place.geometry) {
       window.alert(`No details available for input: ${place.name}`)
@@ -48,8 +55,39 @@ export default class extends Controller {
     this.marker.setPosition(place.geometry.location)
     this.marker.setVisible(true)
 
-    this.latitudeTarget.value = place.geometry.location.lat()
-    this.longitudeTarget.value = place.geometry.location.lng()
+    //-------- test --------
+    let country = null
+    let city = null
+    let lenn = place.address_components.length
+    for (let i = 0; i < lenn; i++){
+      let comp = place.address_components[i]
+      if(comp.types[0] == 'locality'){
+        city = comp.long_name
+      }
+      if(comp.types[0] == 'country'){
+        country = comp.long_name
+      }
+
+    }
+
+    this.latitudeTarget.value = city
+    this.longitudeTarget.value =  country
+    //console.log("City: " + city + ", Country: " + country);
+    //console.log("lenn: " + lenn)
+
+    /* if(1 < lenn && lenn < 2){
+      this.latitudeTarget.value = place.address_components[0]['long_name']
+      this.longitudeTarget.value = null
+    } else {
+      //this.latitudeTarget.value = place.geometry.location.lat()
+      this.latitudeTarget.value = place.address_components[0]['long_name']
+
+      //this.longitudeTarget.value = place.geometry.location.lng()
+      //place.pllace_id
+      this.longitudeTarget.value = place.address_components[lenn -1]['long_name']
+
+    }  */
+    
   }
 
   keydown(event) {
