e29c20086230bbfa3ff58c255ad4c1792a6f824e
Marcoxmas <marco.nat2001@gmail.com>
2023-07-11 17:40:27 +0200

    lista utenti con cambio tipo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authorize_admin, only: [:index]
     def index
       @users = User.all
     end
+
+    def toggle_user_type
+      @user = User.find(params[:id])
+      @user.update(user_type: (@user.user_type == 'regular_user') ? 'admin' : 'regular_user')
+      redirect_to users_path, notice: 'User type toggled successfully.'
+    end
+
+    private
+    def authorize_admin
+      unless current_user.user_type == 'admin'
+        redirect_to root_path, alert: "You are not authorized to access this page."
+      end
+    end
   end
+  
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_12_132710) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_134849) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -89,6 +89,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_12_132710) do
     t.string "name"
     t.string "uid"
     t.string "provider"
+    t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
