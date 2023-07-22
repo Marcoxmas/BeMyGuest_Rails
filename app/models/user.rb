2738fc37cd16a5a86cb0bcb29c894ea0c46e44ab
Gianluca Sanfilippo <sanfilippo.1943663@studenti.uniroma1.it>
2023-07-22 22:51:06 +0200

    implementata funzionalità devise:confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :houses
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :validatable, :confirmable,
          :omniauthable, omniauth_providers: [:google_oauth2]
   
 
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address =>    "smtp.gmail.com",
+    :port    =>   587,
+    :user_name =>  "trial.shopify1@gmail.com",
+    :password  =>  Rails.application.credentials[:gmailpw],
+    :authentication => "plain",
+
+    :enable_starttls_auto => true
+  }
+
